6d592b8b085d35f78018fdf625e69df3e5f1668e
ateforf <atefateforf@gmail.com>
2023-12-19 16:25:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PlayersList from "./PlayersList"; // Assuming PlayersList component is in "./PlayersList"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FIFA Card</h1>
+      
+      <PlayersList />
+    </div>
+
+  );
+}
+
+export default App;
+
---
+++ src/players.js
@@ -0,0 +1,44 @@
+// src/Data.js
+const Players = [
+    {
+      name: "messi",
+      team: "paris",
+      nationality: "tatouine",
+      jerseyNumber: "11",
+      age: "31",
+      image: require('./assets/1.jpeg'), // Update the import path
+    },
+    {
+      name: "cr7",
+      team: "Bayern Munchen",
+      nationality: "mednini",
+      jerseyNumber: "17",
+      age: "31",
+      image: require('./assets/2.jpeg'), // Update the import path
+    },
+    {
+      name: "mpappe",
+      team: "Manchester City",
+      nationality: "Algeria",
+      jerseyNumber: "26",
+      age: "32",
+      image: require('./assets/4.jpeg'), // Update the import path
+    },
+    {
+      name: "Neymar",
+      team: "Paris Saint-Germain",
+      nationality: "Brazil",
+      jerseyNumber: "2",
+      age: "24",
+      image: require('./assets/4.jpeg'), // Update the import path
+    },
+  ];
+  
+  export default Players;
+  
+
+
+
+
+
+  
